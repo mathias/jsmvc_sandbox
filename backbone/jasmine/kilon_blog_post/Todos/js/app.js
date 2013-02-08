269a134b07414f754ed9048a470c82dea337c6e3
Matt Gauger <matt.gauger@gmail.com>
2013-02-07 21:59:09 -0600

    Get app.js and index.html talking

--- backbone/jasmine/kilon_blog_post/Todos/js/app.js
+++ backbone/jasmine/kilon_blog_post/Todos/js/app.js
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: "/js/",
+  baseUrl: "./js/",
   paths: {
     jquery: 'lib/jquery-1.9.1',
     underscore: 'lib/underscore',
@@ -26,19 +26,32 @@ require([
     'backbone',
     'models/Todo',
     'views/MasterView'
-  ], function($, Backbone, Todo, MasterView) {
- 
+  ], function($, Backbone, Todo, MasterView ) {
+
   var Router = Backbone.Router.extend({
     routes: {
       "": "main"
     },
+
     main: function(){
       var tasks = new Todo.Collection();
       var view = new MasterView({collection: tasks});
       tasks.fetch({
         success: function(tasks){
           $("#container").html(view.render().el).show();
+        },
+        error: function(model, error) {
+          // TODO: handle errors nicer
+          alert(error);
         }
       });
     }
   });
+
+  // Preload CSS Sprite
+  $('<img/>').attr('src', "./css/glyphicons.png");
+
+  var router = new Router();
+  Backbone.history.start();
+
+});
