a53f7ac9828a568285e62bf12de6791ac721b787
Matt Gauger <matt.gauger@gmail.com>
2013-02-07 21:33:02 -0600

    Start setting up Require.js-based Backbone app

---
+++ backbone/jasmine/kilon_blog_post/js/app.js
@@ -0,0 +1,44 @@
+require.config({
+  baseUrl: "/js/",
+  paths: {
+    jquery: 'lib/jquery-1.9.1',
+    underscore: 'lib/underscore',
+    backbone: 'lib/backbone',
+    'backbone.localStorage': 'lib/backbone.localStorage'
+  },
+  shim: {
+    underscore: {
+      exports: "_"
+    },
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'backbone.localStorage': {
+      deps: ['backbone'],
+      exports: 'Backbone'
+    }
+  }
+});
+
+require([
+    'jquery',
+    'backbone',
+    'models/Todo',
+    'views/MasterView'
+  ], function($, Backbone, Todo, MasterView) {
+ 
+  var Router = Backbone.Router.extend({
+    routes: {
+      "": "main"
+    },
+    main: function(){
+      var tasks = new Todo.Collection();
+      var view = new MasterView({collection: tasks});
+      tasks.fetch({
+        success: function(tasks){
+          $("#container").html(view.render().el).show();
+        }
+      });
+    }
+  });
